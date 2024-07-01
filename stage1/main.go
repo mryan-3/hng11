2c29ee6fc16d6872fff999289edee7fb005d2491
mryan-3 <malawaryan@gmail.com>
2024-07-01 22:57:55 +0300

    feat(core): implement web server with location and ip

--- stage1/main.go
+++ stage1/main.go
@@ -1,40 +1,93 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	owm "github.com/briandowns/openweathermap"
+	"io"
+	"net/http"
+    "os"
+    "github.com/joho/godotenv"
 	"github.com/gofiber/fiber/v2"
 	"github.com/jpiontek/go-ip-api"
-	"os"
 )
 
-var apiKey = os.Getenv("OWM_API_KEY")
+func init() {
+    err := godotenv.Load()
+    if err != nil {
+        panic("Error loading .env file")
+    }
+}
+
+type WeatherData struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+}
+
+func getWeatherData(city string) (WeatherData, error) {
+	q := city
+
+    apiKey := os.Getenv("WEATHER_API_KEY")
+    if apiKey == "" {
+        return WeatherData{}, fmt.Errorf("WEATHER_API_KEY environment variable not set")
+    }
+
+	url := "http://api.weatherapi.com/v1/forecast.json?key=" + apiKey + "&q=" + q + "&days=1&aqi=no&alerts=no"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return WeatherData{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+	}
+	var data WeatherData
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return WeatherData{}, err
+	}
+
+	return data, nil
+}
 
 func main() {
+
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		client := goip.NewClient()
-		location, err := client.GetLocation()
+
+	app.Get("/api/hello", func(c *fiber.Ctx) error {
+		visitorName := c.Query("visitor_name")
+
+		clientIp := c.IP()
+
+		location, err := goip.NewClient().GetLocation()
 		if err != nil {
 			return c.Status(500).SendString("Error getting location: " + err.Error())
 		}
 
-		if location.City == "" {
-			return c.Status(500).SendString("City not found")
+		weather, _ := getWeatherData(location.City)
+		temperature := weather.Current.TempC
+
+		greeting := fmt.Sprintf("Hello, %s! The temperature is %.2f°C in %s.", visitorName, temperature, location.City)
+
+		response := fiber.Map{
+			"client_ip": clientIp,
+			"location":  location.City,
+			"greeting":  greeting,
 		}
 
-		fmt.Println("City:", location.City)
-		fmt.Println("API Key:", apiKey)
+		return c.JSON(response)
 
-		weather, err := owm.NewCurrent("C", "EN", apiKey)
-		if err != nil {
-			return c.Status(500).SendString("Error initializing weather: " + err.Error())
-		}
-
-		weatherData := weather.CurrentByName(location.City)
-
-		fmt.Println("Weather:", weatherData)
-		return c.SendString(fmt.Sprintf("Weather in %s: %v", location.City, weatherData))
 	})
 
 	app.Listen(":5000")
